ed6edec199b9ab1fb8ff0fd9d4a42fdd322fc569
AzzahraRST <136779259+AzzahraRST@users.noreply.github.com>
2024-12-24 10:25:51 +0700

    Add files via upload

---
+++ storage/framework/views/02d57a10542b80732314714416504a21.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MindfulMedia</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/css/master.css">
+    
+</head>
+<body>
+     <!-- navbar -->
+     <nav class="navbar navbar-expand-lg navbar-dark warna5" style="background-color:#633971">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-4 mt-1">
+                        <img src="/image/logo1.png" style="height: 45px;" alt="">
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/">Home</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/projects">Job</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/forms">Forms</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/faq">FAQ</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/recruitments/create">Recruitments</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="dropdown">
+                <button style="font-size:14px;" class="btn btn-secondary dropdown-toggle warna3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Login
+                </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/recruitments">Admin</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+
+
+    <!-- banner -->
+    <div class="container-fluid banner d-flex align-items-center">
+        <div style="margin-left: 200px;" class="container text-left text-light">
+            <h1 style="font-size: 47px; font-family: arial;"><strong>Pendaftaran Pegawai Desa Dawu, Kabupaten Ngawi</strong></h1>
+            <button type="button" class="btn btn-primary" style="background-color: #633971;"><a class="link" style="text-decoration: none;" href="/recruitments/create">Daftar</a></button>
+        </div>
+        <div class="container text-white">
+            <img src="/image/hero.png" alt="" style="height: 650px;">
+            <div class="col-md-8 offset-md-2">
+            </div>
+        </div>
+    </div>
+    
+     <!-- bio -->
+     <div class="text-center py-5">
+            <h3 class="h3">About Us</h3>
+
+            <section class="about">
+                <div class="row">
+                    
+                    <div class="content">
+                        <h3 style="font-size: 30px; color: #633971;">Description<br></h3>
+                        <p align="justify" style="font-size: 20px;">
+                           <strong> Balai Desa Dawu :</strong><br> 
+                           Desa Dawu, yang terletak di Kecamatan Paron, Kabupaten Ngawi, Jawa Timur, adalah desa agraris dengan mayoritas penduduk bermata pencaharian sebagai petani dan pekebun, menghasilkan komoditas utama seperti padi dan jagung. Dengan lingkungan yang asri, tradisi gotong-royong yang kuat, serta infrastruktur yang terus berkembang, desa ini memiliki potensi besar di bidang pertanian, UMKM, dan agrowisata. Pemerintahan Desa Dawu juga berupaya meningkatkan pelayanan publik melalui inovasi digital, seperti pendataan penduduk berbasis website, guna mendukung pengelolaan data yang lebih efektif dan pembangunan desa yang mandiri serta sejahtera.<br>
+                        </p>
+                        <h3 style="font-size: 30px; color: #633971;">VISI & MISI</h3>
+                        <p align="justify" style="font-size: 20px;">
+                            <strong>VISI</strong><br>
+                            "Senggigi Berseri"
+(Bersih, Relegius, Sejahtera, Rapi, dan Indah)
+"Terwujudnya masyarakat Desa Senggigi yang Bersih, Relegius, Sejahtra, Rapi dan Indah melalui Akselerasi Pembangunan yang berbasis Keagamaan, Budaya Hukum dan Berwawasan Lingkungan dengan berorentasi pada peningkatan Kinerja Aparatur dan Pemberdayaan Masyarakat"<br>
+                            <strong>MISI</strong><br>
+                            1. Pembangunan Jangka Panjang<br>
+                            - Melanjutkan Pembangunan Desa yang belum terlaksana<br>
+                            - Meningkatkan Kerjasama antara pemerintah Desa dengan Lembaga desa yang ada<br>
+                            - Meningkatkan Kesejahtraan Masyarakat desa dengan meningkatkan sarana dan prasarana ekonomi warga.<br>
+                            2. Pembangunan Jangka Pendek <br>
+                            - Mengembangkan dan Menjaga serta melestarikan ada istiadat desa terutama yang telah mengakar di desa senggigi. <br>
+                            - Meningkatkan pelayanan dalam bidang pemerintahan kepada warga masyarakat <br>
+                            - Meningkatkan sarana dan prasarana ekonomi warga desa dengan perbaikan prasarana dan sarana ekonomi.<br>
+                            - Meningkatkan sarana dan prasarana pendidikan guna peningkatan sumber daya manusia desa senggigi.<br>
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3 class="h3 py-3">Editing Video Example</h3>
+                <div class="video">
+                    <video id="video" controls loop autoplay src="video/uwu.mp4" style="width: 70%;"></video>
+                </div>
+        </div>
+</div>
+</div>
+
+     <!-- bio -->
+     <div class="text-center py-5">
+        <h3 style="font-size: 30px; color: #633971;" class="h3">MindfullMedia</h3>
+
+        <section class="about">
+            <div class="row">
+                <div class="content">
+                    <h3 style="font-size: 30px; color: #633971;">Tim Kami<br></h3>
+                    <p align="justify" style="font-size: 20px;">
+                    Kami memiliki tim yang terdiri dari editor video, desainer presentasi, ilustrator dan retoucher foto yang berbakat dan berdedikasi dalam menciptakan karya-karya 
+                    berkualitas tinggi. Setiap anggota tim kami memiliki pengetahuan yang mendalam tentang tool - tool, gaya visual, dan pengalaman dalam industri ini. Kami berkomitmen untuk memberikan hasil yang memenuhi harapan Anda.
+                    </p>
+                    <h3 style="font-size: 30px; color: #633971;">some words that describe MindfullMedia</h3>
+                    <p align="justify" style="font-size: 20px;">
+                        Kreativitas Tanpa Batas: Kami percaya bahwa dalam dunia video, hanya imajinasi yang dapat membatasi apa yang dapat kita capai. Dalam setiap proyek, kami berusaha 
+                        untuk mengeksplorasi batas-batas kreativitas dan memberikan hasil yang menginspirasi.<br><br>
+                        Berkualitas dan Profesionalisme: Setiap file proyek video yang kami tawarkan dan setiap proyek editan yang kami selesaikan mengutamakan kualitas tinggi dan profesionalisme. 
+                        Kami menghargai setiap detail dan berusaha untuk memberikan hasil yang memenuhi harapan Anda.
+                    </p>
+                </div>
+                <div class="images mt-3 mb-3">
+                    <img src="/image/logo1.png" alt="">
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- highlighted kategori --> 
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3 class="h3">Job We Offer</h3><br>
+            <p align="justify" style="font-size: 19px; color: #633971;">Sebagai seorang web freelancer yang berfokus pada multimedia, Anda akan dituntut untuk menguasai sejumlah skill yang berhubungan dengan elemen-elemen multimedia seperti gambar, audio, video, dan animasi. Berikut adalah beberapa kata-kata deskripsi untuk skill yang diperlukan dalam web freelance multimedia:<br><br>
+            <strong>1. Desain Grafis (Graphic Design):</strong> <br>Kemampuan untuk menciptakan elemen grafis yang menarik dan estetis, termasuk logo, banner, ilustrasi, dan grafik lainnya yang akan meningkatkan tampilan visual situs web.<br><br>
+            <strong>2. Pengeditan Foto dan Gambar (Photo and Image Editing):</strong> <br>Mampu mengedit dan memanipulasi foto dan gambar agar tampil lebih profesional dan sesuai dengan kebutuhan proyek.<br><br>
+            <strong>3. Pengembangan Audio (Audio Development):</strong> <br>Kemampuan untuk mengintegrasikan elemen audio seperti musik, suara latar, atau efek suara agar situs web lebih menarik dan interaktif.<br><br>
+            <strong>4. Pengeditan Video (Video Editing):</strong> <br>Menguasai teknik pengeditan video untuk menciptakan konten video yang menarik dan relevan dengan konten situs web.<br><br>
+            <strong>5. Animasi (Animation):</strong> <br> Mampu menciptakan animasi grafis atau animasi dalam bentuk video untuk menambahkan elemen dinamis dan interaktif pada situs web.<br><br>
+
+            </p>
+
+            <div class="row mt-5">
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-keahlian keahlian-satu d-flex justify-content-center align-items-center">
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-keahlian keahlian-dua d-flex justify-content-center align-items-center">
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-keahlian keahlian-tiga d-flex justify-content-center align-items-center">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3 class="h3 py-3">My After Effect Project</h3>
+                <div class="video">
+                    <video id="video" controls loop src="video/elaina.mp4" style="width: 70%;"></video>
+                </div>
+        </div>
+    </div>
+</div>
+    
+    <!-- Footer -->
+    <div class="container-fluid py-5 warna5 text-light">
+        <div class="container d-flex justify-content-between">
+            <label>Follow Us:</label>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="https://www.instagram.com/izumiidesu/?hl=id"><i class="fab fa-instagram fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="https://twitter.com"><i class="fab fa-twitter fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="wa.me/6289524309404"><i class="fab fa-whatsapp fs-2"></i></a>
+                </div>
+        </div>
+    </div>
+    
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+    <script>
+        window.onload = function () {
+        var element = document.getElementById('video');
+        element.muted = "muted";}
+    </script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\minfulmedia-main\resources\views/rumah.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/6e8fa6edd56e74bc9f01c96ab9487d05.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ PAGE</title>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/css/faq.css">
+</head>
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark warna5" style="background-color:#633971">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-4 mt-1">
+                        <img src="/image/logo1.png" style="height: 45px;" alt="">
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/">Home</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/projects">Job</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/forms">Forms</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/faq">FAQ</a>
+                    </li>
+                    <li class="nav-item me-4">
+                    <a class="nav-link" style="color:#F5E9CF;" href="/recruitments/create">Recruitments</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
+    <!-- banner -->
+    <div class="container-fluid banner d-flex align-items-center">
+        <div style="margin-left: 200px;" class="container text-left text-light">
+            <h1 style="font-size: 47px; font-family: arial;"><strong>Make your best portfolio with Us</strong></h1>
+        </div>
+        <div class="container text-white">
+            <img src="/image/hero.png" alt="" style="height: 650px;">
+            <div class="col-md-8 offset-md-2">
+            </div>
+        </div>
+    </div>
+
+    <!-- Section: FAQ -->
+    <section>
+        <h1 class="text-center mb-4 pb-2 warna6 fw-bold">FAQ</h1>
+        <h2 style="font-size: 25px;" class="text-center mb-5 warna6">
+            Find the answers for the most frequently asked questions below
+        </h2>
+        
+        
+        
+        <br><br>
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="far fa-paper-plane warna6 pe-2"></i> Apa saja jenis proyek multimedia yang bisa saya lamar?</h4>
+                <p style="font-size: 20px;" align="justify">
+                    Desain Grafis: Desain logo, brosur, pamflet, poster, dan materi pemasaran lainnya.
+                    Animasi: Pembuatan animasi 2D atau 3D untuk iklan, video musik, atau presentasi.
+                    Video Editing: Mengedit video untuk acara, promosi, atau konten online.
+                    Fotografi: Mengambil gambar untuk acara, produk, atau potret.
+                    Audio dan Musik: Produksi musik, editing audio, atau voice-over untuk video.
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="fas fa-pen-alt warna6 pe-2"></i> Apa saja keahlian dan kualifikasi yang diperlukan?</h4>
+                <p style="font-size: 20px;" align="justify">
+                    Namun, dalam bidang multimedia, kami mencari freelancer dengan kemampuan dan pengalaman dalam 
+                    perangkat lunak kreatif seperti Adobe Creative Suite (Photoshop, Illustrator, Premiere Pro, After Effects), Blender (untuk animasi 3D), DAW (Digital Audio Workstation) untuk produksi musik, dan peralatan fotografi profesional.
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="fas fa-user warna6 pe-2"></i>Bagaimana cara melamar untuk proyek multimedia?
+                </h4>
+                <p style="font-size: 20px;" align="justify">
+                    Anda dapat melamar untuk proyek multimedia dengan membuka halaman proyek terkait di situs kami dan mengirimkan proposal sesuai dengan instruksi yang tertera. 
+                    Pastikan untuk menyertakan portofolio Anda dan rincian pengalaman relevan untuk mendukung aplikasi Anda.
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="fas fa-book-open warna6 pe-2"></i>Apakah ada kesempatan untuk kolaborasi jangka panjang?</h4>
+                <p style="font-size: 20px;" align="justify">
+                    Tidak, kami menyambut freelancer multimedia dari seluruh dunia. Sebagai platform rekrutmen freelance online, kami membuka kesempatan bagi siapa saja dengan kualifikasi yang sesuai dan kemampuan yang dibutuhkan.
+                </p>
+            </div>
+            
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="fas fa-rocket warna6 pe-2"></i>Apakah saya perlu membayar biaya pendaftaran?
+                </h4>
+                <p style="font-size: 20px;" align="justify">
+                    Kami tidak membebankan biaya apapun untuk mendaftar atau melamar proyek. 
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h4 class="mb-3 warna6"><i class="fas fa-home warna6 pe-2"></i>Berapa lama waktu respon setelah mengirimkan proposal?
+                </h4>
+                <p style="font-size: 20px;" align="justify">Kami berusaha untuk merespon setiap proposal dalam waktu yang wajar. Namun, waktu respon dapat bervariasi tergantung pada volume aplikasi yang kami terima dan kompleksitas proyek. Kami akan memberi tahu Anda secepat mungkin. 
+                </p>
+            </div>
+        </div>
+    </section>
+<!-- Section: FAQ -->
+
+
+    
+    <!-- Footer -->
+    <div class="container-fluid py-5 warna5 text-light">
+        <div class="container d-flex justify-content-between">
+            <label>Follow Us:</label>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="https://www.instagram.com/izumiidesu/?hl=id"><i class="fab fa-instagram fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="https://twitter.com"><i class="fab fa-twitter fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a class="link" href="wa.me/6289524309404"><i class="fab fa-whatsapp fs-2"></i></a>
+                </div>
+        </div>
+    </div>
+    
+    <script src="/js/faq.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html><?php /**PATH /var/www/resources/views/faq.blade.php ENDPATH**/ ?>
